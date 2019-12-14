22d3e9455af2ac0f4d27195ee559b0c3e7eeec74
JojoS62 <sn2010@onlinehome.de>
2019-12-14 15:52:02 +0100

    removed network-helper

--- source/main.cpp
+++ source/main.cpp
@@ -1,6 +1,6 @@
 #include "mbed.h"
 
-#include "network-helper.h"
+//#include "network-helper.h"
 #include "HttpServer.h"
 #include "HttpResponseBuilder.h"
 #include "HttpParsedRequest.h"
@@ -225,7 +225,7 @@ int main() {
     
     // Connect to the network with the default networking interface
     // if you use WiFi: see mbed_app.json for the credentials
-    NetworkInterface* network = connect_to_default_network_interface();
+    NetworkInterface* network = NetworkInterface::get_default_instance();
     if (!network) {
         printf("Cannot connect to the network, see serial output\n");
         return 1;
@@ -244,7 +244,9 @@ int main() {
     nsapi_error_t res = server.start(8080);
 
     if (res == NSAPI_ERROR_OK) {
-        printf("Server is listening at http://%s:8080\n", network->get_ip_address());
+        SocketAddress socketAddress;
+        network->get_ip_address(&socketAddress);
+        printf("Server is listening at http://%s:8080\n", socketAddress.get_ip_address());
     }
     else {
         printf("Server could not be started... %d\n", res);
