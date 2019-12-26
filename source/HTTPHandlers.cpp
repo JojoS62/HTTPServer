a2ae913408a6eab6c55e8d7128927f50bf439e63
JojoS62 <sn2010@onlinehome.de>
2019-12-26 15:47:43 +0100

    added testSVG

--- source/HTTPHandlers.cpp
+++ source/HTTPHandlers.cpp
@@ -1,6 +1,10 @@
 #include "HTTPhandlers.h"
 #include "globalVars.h"
 
+#define SAMPLE_TIME     1000 // milli-sec
+#define COMPLETED_FLAG (1UL << 0)
+EventFlags threadFlag;
+
 PlatformMutex stdio_mutex;
 
 void print_stats()
@@ -76,6 +80,24 @@ void request_handler(HttpParsedRequest* request, ClientConnection* clientConnect
         builder.sendBodyString(body);
     } else if(request->get_method() == HTTP_GET && request->get_url() == "/") {
         builder.sendHeaderAndFile(&fs, "/index.html");
+    } else if(request->get_method() == HTTP_GET && request->get_url() == "/test.svg") {
+        string out;
+        out.reserve(2048);
+        char temp[100];
+
+        out += "<svg xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\" width=\"400\" height=\"150\">\n";
+        out += "<rect width=\"400\" height=\"150\" fill=\"rgb(250, 230, 210)\" stroke-width=\"1\" stroke=\"rgb(0, 0, 0)\" />\n";
+        out += "<g stroke=\"black\">\n";
+        int y = rand() % 130;
+        for (int x = 10; x < 390; x+= 10) {
+            int y2 = rand() % 130;
+            sprintf(temp, "<line x1=\"%d\" y1=\"%d\" x2=\"%d\" y2=\"%d\" stroke-width=\"1\" />\n", x, 140 - y, x + 10, 140 - y2);
+            out += temp;
+            y = y2;
+        }
+        out += "</g>\n</svg>\n";
+        builder.sendHeader (200);
+        builder.sendBodyString(out);
     } else if(request->get_method() == HTTP_GET) {
         builder.sendHeaderAndFile(&fs, request->get_url());
     } else if (request->get_method() == HTTP_POST && request->get_url() == "/toggle") {
